10a8e95c10590d1fea12ab0507eff66a46a1dab2
Ming-D-W <wuwanming0405@163.com>
2022-12-31 09:24:10 +0800

    day4

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="color">color</div>
+    <div class="color1">color1</div>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+<style lang = 'less'>
+#app{
+  .color{
+    color: red;
   }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  .color1{
+    color: green;
+  }
 }
 </style>
