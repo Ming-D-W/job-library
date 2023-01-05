0d9c7d5625a97bd3fcd8aa7439a494facedc90ff
Ming-D-W <wuwanming0405@163.com>
2023-01-05 10:04:13 +0800

    11

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -2,13 +2,13 @@
   <div>
     <!-- 底部部分 -->
     <footer class="footer">
-      <span class="todo-count" ><strong>{{remainder}}</strong>剩余</span>
+      <span class="todo-count" ><strong>0</strong>剩余</span>
       <ul class="filters">
         <li>
           <a class="selected" href="#/">全部</a>
         </li>
         <li>
-          <a href="#/active" @click="haveInHand">进行中</a>
+          <a href="#/active">进行中</a>
         </li>
         <li>
           <a href="#/completed">已完成</a>
@@ -34,21 +34,10 @@ export default {
     return {}
   },
   methods: {
-    haveInHand(){
-      const a= this.list.filter(item=>item.isDone===true)
-      console.log(a)
-      console.log(this.list)
-      this.list=a
-    }
+
   },
   computed: {
-    remainder(){
-      return this.list.filter(item=>item.isDone===false).length
-    },
-    // haveInHand(){
-    //   const a= this.list.filter(item=>item.isDone===true)
-    //   console.log(a)
-    // }
+
   }
 }
 </script>
