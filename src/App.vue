a3c93e0423dd01f74a30cc223e5c8cb5c3c83c7d
Ming-D-W <wuwanming0405@163.com>
2023-01-08 23:41:31 +0800

    1.8作业

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,132 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="box">
+    <Card :list="list_1">
+      <template #left="{item}">
+        <img :src="item.url" alt="">
+        {{ item.name }}
+      </template>
+      <template #right="{item}">
+        {{ item.count }}
+      </template>
+    </Card>
+    <Card :list="list_2">
+      <template #title>
+        <h4 class="title">本周热搜 TOP5</h4>
+      </template>
+      <template #left="{item}">
+        #{{ item.name }}
+      </template>
+      <template #right="{item}">
+        <img src="./assets/icon/huo.png" style="width: 20px;margin-bottom: 8px;" alt="">
+        {{ item.count }}
+      </template>
+    </Card>
+    <Card :list="list_3">
+      <template #title>
+        <h4 class="title">可能感兴趣的人</h4>
+      </template>
+      <template #left="{item}">
+        <div style="height: 400px;">
+          {{ item.name }}
+          {{ item.age }}
+          <span>研究领域：{{ item.field }}</span>
+          {{ item.university }}
+          {{ item.position }}
+        </div>
+      </template>
+      <template #right>
+        <button style="width:60px;
+        height: 26px;
+        color: #FFF;
+        background-color:#3780FA;
+        border-radius: 13px">
+          +关注
+        </button>
+      </template>
+    </Card>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from "@/components/card.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "",
+  components: {Card},
+  data() {
+    return {
+      list_1: [
+        {
+          id: 1,
+          url:'./assets/icon/zan.png',
+          name: '我的赞',
+          count: 1299
+        },
+        {
+          id: 2,
+          url:'./assets/icon/star.png',
+          name: '我收藏',
+          count: 399
+        }
+      ],
+      list_2: [
+        {
+          id: 1,
+          name: '区块链',
+          count: 99
+        },
+        {
+          id: 2,
+          name: '数据挖掘',
+          count: 99
+        },
+        {
+          id: 3,
+          name: '无人机',
+          count: 99
+        },
+        {
+          id: 4,
+          name: '生命科学',
+          count: 99
+        },
+        {
+          id: 5,
+          name: '传感器',
+          count: 99
+        }
+      ],
+      list_3: [
+        {
+          id: 1,
+          name: '李国盛',
+          age: '52岁',
+          field: '机电一体化技术',
+          university: '哈尔滨工业大学',
+          position: '教授'
+        },
+        {
+          id: 2,
+          name: '陈颖',
+          age: '36岁',
+          field: '人工智能',
+          university: '清华大学',
+          position: '副教授'
+        },
+      ]
+    }
+  },
+  methods: {},
+  computed: {}
 }
 </script>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped lang="less">
+.box {
+  border: 2px red solid;
+  height: 1000px;
+  width: 400px;
+  background-color: #F4F6FA;
 }
 </style>
---
+++ src/components/card.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="currency">
+    <div class="title" v-if="$slots.title">
+      <slot name="title"></slot>
+    </div>
+    <div class="box">
+      <div class="box_0" v-for="item in list" :key="item.id">
+        <div class="left">
+          <slot name="left" :item="item"></slot>
+        </div>
+        <div class="right">
+          <slot name="right" :item="item"></slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "",
+  props: {
+    list: Array
+  },
+  data() {
+    return {}
+  },
+  methods: {},
+  computed: {}
+}
+</script>
+
+<style scoped lang="less">
+.currency {
+  width: 330px;
+  background-color: #fff;
+  margin: 16px auto;
+  border-radius: 10px;
+  .title {
+    height: 50px;
+    line-height: 50px;
+    padding-left: 20px;
+    color: #fff;
+    font-size: 20px;
+    font-weight: bold;
+    background-image: linear-gradient(to right, #1B79FB, #5DD1FD);
+  }
+
+  .box {
+    .box_0 {
+      display: flex;
+      justify-content: space-between;
+      border-bottom: 1px solid silver;
+      height: 60px;
+      line-height: 60px;
+      width: 270px;
+      margin: 0 auto;
+      .right {
+        font-weight: bold;
+      }
+    }
+    .box_0:last-child {
+      border-bottom: none;
+    }
+  }
+}
+</style>
